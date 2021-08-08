c55cc0dd005b6e36090963ff8b24203304129bf5
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-08-08 05:52:55 -0400

    enums loadedddddd

---
+++ src/main/java/Die.java
@@ -0,0 +1,22 @@
+
+
+public enum Die {
+
+    D4(4),
+    D6(6),
+    D8(8),
+    D10(10),
+    D12(12),
+    D20(20),
+    D100(100);
+
+    private final Integer numberOfSides;
+
+    public Integer getNumberOfSides(){
+        return this.numberOfSides;
+    }
+
+    private Die (Integer numberOfSides){
+        this.numberOfSides =numberOfSides;
+    }
+}
